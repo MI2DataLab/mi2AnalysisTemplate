b66c16a260fed462185483d15ffbecf2b4bc99a7
Mateusz Staniak <mtst@mstaniak.pl>
2019-02-14 00:06:25 +0100

    create R project

--- R/dir_init.R
+++ R/dir_init.R
@@ -1,12 +1,38 @@
 #' Create directory structure for analysis
 #'
 #' @param path path to working directory (not ending in "/")
+#' @param name project name (no spaces or special signs)
 #'
 #' @export
 #'
 
-create_dir_structure <- function(path) {
+create_dir_structure <- function(path, name) {
   dir.create(paste(path, "data", sep = "/"))
   dir.create(paste(path, "misc", sep = "/"))
   dir.create(paste(path, "R", sep = "/"))
+
+  file.create(paste(path, paste0(name, ".Rproj"), sep = "/"))
+  fileProj <- file(paste(path, paste0(name, ".Rproj"), sep = "/"))
+  writeLines(
+    c(
+      "Version: 1.0",
+      "",
+      "RestoreWorkspace: No",
+      "SaveWorkspace: No",
+      "AlwaysSaveHistory: No",
+      "",
+      "EnableCodeIndexing: Yes",
+      "UseSpacesForTab: Yes",
+      "NumSpacesForTab: 2",
+      "Encoding: UTF-8",
+      "",
+      "RnwWeave: Sweave",
+      "LaTeX: pdfLaTeX",
+      "",
+      "AutoAppendNewline: Yes",
+      "StripTrailingWhitespace: Yes"
+    ),
+    fileProj
+  )
+  close.connection(fileProj)
 }
